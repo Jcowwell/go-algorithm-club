9890ee14f2905a53edfeaa7a136dd49ab53c8342
Jcowwell <20213661+Jcowwell@users.noreply.github.com>
2022-02-26 01:06:15 -0500

    small linked list optimization , comment cleanup

--- LinkedList/linked_list.go
+++ LinkedList/linked_list.go
@@ -11,9 +11,9 @@ type LinkedListNode[T any] struct {
 	prev  *LinkedListNode[T] // The head of the Linked List
 }
 
-// Construction Function to return a new LinkedListNode
+// Construction Method to return a new LinkedListNode
 func LinkedListNodeInit[T any](value T) *LinkedListNode[T] {
-	return &LinkedListNode[T]{value: value, next: nil, prev: nil}
+	return &LinkedListNode[T]{value: value}
 }
 
 // Linked List's Struct
@@ -21,56 +21,52 @@ type LinkedList[T any] struct {
 	head *LinkedListNode[T] // The head of the Linked List
 }
 
-// Function to check if list is empty
-func (LL *LinkedList[T]) isEmpty() bool {
-	return LL.head == nil
+// Method to check if list is empty
+func (self *LinkedList[T]) isEmpty() bool {
+	return self.head == nil
 }
 
-// Function to return # of elements in LinkedList
-func (LL *LinkedList[T]) count() int {
-	if LL.head == nil {
-		return 0
+// Method to return # of elements in LinkedList.
+func (self *LinkedList[T]) count() int {
+	if head := self.head; head != nil {
+		count := 1
+		for node := head.next; node != nil; {
+			node = node.next
+			count += 1
+		}
+		return count
 	}
-	count := 1
-	node := LL.head.next
-	for node != nil {
-		node = node.next
-		count += 1
-	}
-	return count
+	return 0
 }
 
-// Function to last LinkedListNode LinkedList
-func (LL *LinkedList[T]) tail() *LinkedListNode[T] {
-	if LL.head == nil {
-		return nil
+// Method to return last LinkedListNode in LinkedList.
+func (self *LinkedList[T]) tail() *LinkedListNode[T] {
+	if head := self.head; head != nil {
+		node := self.head
+		for node.next != nil {
+			node = node.next
+		}
+		return node
 	}
-	node := LL.head
-	for node.next != nil {
-		node = node.next
-	}
-	return node
+	return nil
 }
 
-// Function to return the node at a specific index. Crashes if index is out of bounds (0...self.count)
-//
-// - Parameter index: Integer value of the node's index to be returned
-// - Returns: LinkedListNode
-func (LL *LinkedList[T]) nodeAt(index int) *LinkedListNode[T] {
-	if LL.head == nil {
-		panic("List is empty")
+// Method to return the node at a specific index. Crashes if index is out of bounds (0...self.count).
+func (self *LinkedList[T]) nodeAt(index int) *LinkedListNode[T] {
+	if self.isEmpty() {
+		panic("list is empty")
 	}
 	if index < 0 {
 		panic("index must be greater or equal to 0")
 	}
 
 	if index == 0 {
-		return LL.head
+		return self.head
 	} else {
-		node := LL.head.next
+		node := self.head.next
 		for i := 1; i < index; i++ {
 			node = node.next
-			if node == nil { // might break , put after if so.
+			if node == nil {
 				break
 			}
 		}
@@ -82,52 +78,42 @@ func (LL *LinkedList[T]) nodeAt(index int) *LinkedListNode[T] {
 }
 
 // Append a copy of a LinkedListNode to the end of the list.
-//
-// - Parameter node: The node containing the value to be appended
-func (LL *LinkedList[T]) appendNode(node *LinkedListNode[T]) {
-	if LL.isEmpty() {
-		LL.head = node
+func (self *LinkedList[T]) appendNode(node *LinkedListNode[T]) {
+	if self.isEmpty() {
+		self.head = node
 	} else {
-		lastNode := LL.tail()
+		lastNode := self.tail()
 		node.prev = lastNode
 		lastNode.next = node
 	}
 }
 
-// Append a value to the end of the list
-//
-// - Parameter value: The data value to be appended
-func (LL *LinkedList[T]) appendValue(value T) {
+// Append a value to the end of the list.
+func (self *LinkedList[T]) appendValue(value T) {
 	node := LinkedListNodeInit(value)
-	LL.appendNode(node)
+	self.appendNode(node)
 }
 
-// Append a copy of a LinkedList to the end of the list.
-//
-// - Parameter list: The list to be copied and appended.
-func (LL *LinkedList[T]) appendList(list *LinkedList[T]) {
+// Method to append a copy of a LinkedList to the end of the list.
+func (self *LinkedList[T]) appendList(list *LinkedList[T]) {
 	copyNode := list.head
 	for copyNode != nil {
 		node := copyNode
-		LL.appendValue(node.value)
+		self.appendValue(node.value)
 		copyNode = node.next
 	}
 }
 
-// Insert a copy of a node at a specific index. Crashes if index is out of bounds (0...self.count)
-//
-// 	 - Parameters:
-//   - node: The node containing the value to be inserted
-//   - index: Integer value of the index to be inserted at
-func (LL *LinkedList[T]) insertNode(node *LinkedListNode[T], index int) {
-	if LL.isEmpty() && index == 0 {
-		LL.head = node
+// Method to insert a copy of a node at a specific index. Crashes if index is out of bounds (0...self.count).
+func (self *LinkedList[T]) insertNode(node *LinkedListNode[T], index int) {
+	if self.isEmpty() && index == 0 {
+		self.head = node
 	} else if index == 0 {
-		node.next = LL.head
-		LL.head.prev = node
-		LL.head = node
+		node.next = self.head
+		self.head.prev = node
+		self.head = node
 	} else {
-		prev := LL.nodeAt(index - 1)
+		prev := self.nodeAt(index - 1)
 		next := prev.next
 		node.prev = prev
 		node.next = next
@@ -136,31 +122,23 @@ func (LL *LinkedList[T]) insertNode(node *LinkedListNode[T], index int) {
 	}
 }
 
-// Insert a value at a specific index. Crashes if index is out of bounds (0...self.count)
-//
-// - Parameters:
-//   - value: The data value to be inserted
-//   - index: Integer value of the index to be insterted at
-func (LL *LinkedList[T]) insertValue(value T, index int) {
+// Method to insert a value at a specific index. Crashes if index is out of bounds (0...self.count).
+func (self *LinkedList[T]) insertValue(value T, index int) {
 	node := LinkedListNodeInit(value)
-	LL.insertNode(node, index)
+	self.insertNode(node, index)
 }
 
-// Insert a copy of a LinkedList at a specific index. Crashes if index is out of bounds (0...self.count)
-//
-// - Parameters:
-//   - list: The LinkedList to be copied and inserted
-//   - index: Integer value of the index to be inserted at
-func (LL *LinkedList[T]) insertList(list *LinkedList[T], index int) {
-	if LL.isEmpty() {
+// Method to insert a copy of a LinkedList at a specific index. Crashes if index is out of bounds (0...self.count).
+func (self *LinkedList[T]) insertList(list *LinkedList[T], index int) {
+	if self.isEmpty() {
 		return
 	}
 
 	if index == 0 {
-		list.tail().next = LL.head
-		LL.head = list.head
+		list.tail().next = self.head
+		self.head = list.head
 	} else {
-		prev := LL.nodeAt(index - 1)
+		prev := self.nodeAt(index - 1)
 		next := prev.next
 
 		prev.next = list.head
@@ -173,23 +151,20 @@ func (LL *LinkedList[T]) insertList(list *LinkedList[T], index int) {
 	}
 }
 
-// Function to remove all nodes/value from the list
-func (LL *LinkedList[T]) removeAll() {
-	LL.head = nil
+// Method to remove all nodes/value from the list.
+func (self *LinkedList[T]) removeAll() {
+	self.head = nil
 }
 
-// Function to remove a specific node.
-//
-// - Parameter node: The node to be deleted
-// - Returns: The data value contained in the deleted node.
-func (LL *LinkedList[T]) removeNode(node *LinkedListNode[T]) T {
+// Method to remove a specific node.
+func (self *LinkedList[T]) removeNode(node *LinkedListNode[T]) T {
 	prev := node.prev
 	next := node.next
 
 	if prev != nil {
 		prev.next = next
 	} else {
-		LL.head = next
+		self.head = next
 	}
 	if next != nil {
 		next.prev = prev
@@ -200,41 +175,34 @@ func (LL *LinkedList[T]) removeNode(node *LinkedListNode[T]) T {
 	return node.value
 }
 
-// Function to remove the last node/value in the list. Crashes if the list is empty
-//
-// - Returns: The data value contained in the deleted node.
-func (LL *LinkedList[T]) removeLast() T {
-	if LL.isEmpty() {
+// Method to remove the last node/value in the list. Crashes if the list is empty.
+func (self *LinkedList[T]) removeLast() T {
+	if self.isEmpty() {
 		panic("List is empty")
 	}
-	return LL.removeNode(LL.tail())
+	return self.removeNode(self.tail())
 }
 
-// Function to remove a node/value at a specific index. Crashes if index is out of bounds (0...self.count)
-//
-// - Parameter index: Integer value of the index of the node to be removed
-// - Returns: The data value contained in the deleted node
-func (LL *LinkedList[T]) removeAt(index int) T {
-	node := LL.nodeAt(index)
-	return LL.removeNode(node)
+// Method to remove a node/value at a specific index. Crashes if index is out of bounds (0...self.count).
+func (self *LinkedList[T]) removeAt(index int) T {
+	node := self.nodeAt(index)
+	return self.removeNode(node)
 }
 
-// Function to reverse list
-func (LL *LinkedList[T]) reverse() {
-	node := LL.head
-	for node != nil {
+// Method to reverse list.
+func (self *LinkedList[T]) reverse() {
+	for node := self.head; node != nil; {
 		currentNode := node
 		node = currentNode.next
 		Swap(&currentNode.next, &currentNode.prev)
-		LL.head = currentNode
+		self.head = currentNode
 	}
 }
 
-// Function to filter list
-func (LL *LinkedList[T]) Filter(predicate Predicate[T]) *LinkedList[T] {
+// Method to filter list.
+func (self *LinkedList[T]) filter(predicate Predicate[T]) *LinkedList[T] {
 	result := &LinkedList[T]{}
-	node := LL.head
-	for node != nil {
+	for node := self.head; node != nil; {
 		temp_node := node
 		if predicate(node.value) {
 			result.appendValue(node.value)
@@ -244,11 +212,10 @@ func (LL *LinkedList[T]) Filter(predicate Predicate[T]) *LinkedList[T] {
 	return result
 }
 
-// Function to map LinkedList of Node Elements T to LinkedList of Node Elements U
-func LinkedListMap[T, U any](transform Transform[T, U], list *LinkedList[T]) *LinkedList[U] {
+// Static Function to map LinkedList of Node Elements T to LinkedList of Node Elements U.
+func MapLinkedList[T, U any](transform Transform[T, U], list *LinkedList[T]) *LinkedList[U] {
 	result := &LinkedList[U]{}
-	node := list.head
-	for node != nil {
+	for node := list.head; node != nil; {
 		temp_node := node
 		result.appendValue(transform(node.value))
 		node = temp_node.next
