5a8411d55be1dbea320139e7dec0f26a22143eaa
Jcowwell <20213661+Jcowwell@users.noreply.github.com>
2022-03-01 09:14:14 -0500

    bug fix: caplilized method names to make them public for out of package use.

--- Stack/stack_test.go
+++ Stack/stack_test.go
@@ -8,83 +8,83 @@ import (
 
 func TestEmpty(t *testing.T) {
 	stack := Stack[int]{}
-	AssertTrue(stack.isEmpty(), t)
-	AssertEqual(stack.count(), 0, t)
-	_, validPeek := stack.peek()
+	AssertTrue(stack.IsEmpty(), t)
+	AssertEqual(stack.Count(), 0, t)
+	_, validPeek := stack.Peek()
 	AssertFalse(validPeek, t)
-	_, validPop := stack.pop()
+	_, validPop := stack.Pop()
 	AssertFalse(validPop, t)
 }
 
 func TestOneElement(t *testing.T) {
 	stack := Stack[int]{}
 
-	stack.push(123)
-	AssertFalse(stack.isEmpty(), t)
-	AssertEqual(stack.count(), 1, t)
-	valuePeek, _ := stack.peek()
+	stack.Push(123)
+	AssertFalse(stack.IsEmpty(), t)
+	AssertEqual(stack.Count(), 1, t)
+	valuePeek, _ := stack.Peek()
 	AssertEqual(valuePeek, 123, t)
 
-	valuePop, _ := stack.pop()
+	valuePop, _ := stack.Pop()
 	AssertEqual(valuePop, 123, t)
-	AssertTrue(stack.isEmpty(), t)
-	AssertEqual(stack.count(), 0, t)
-	_, validPeek := stack.peek()
+	AssertTrue(stack.IsEmpty(), t)
+	AssertEqual(stack.Count(), 0, t)
+	_, validPeek := stack.Peek()
 	AssertFalse(validPeek, t)
-	_, validPop := stack.pop()
+	_, validPop := stack.Pop()
 	AssertFalse(validPop, t)
 }
 
 func TestTwoElements(t *testing.T) {
 	stack := Stack[int]{}
 
-	stack.push(123)
-	stack.push(456)
-	AssertFalse(stack.isEmpty(), t)
-	AssertEqual(stack.count(), 2, t)
-	valuePeek, _ := stack.peek()
+	stack.Push(123)
+	stack.Push(456)
+	AssertFalse(stack.IsEmpty(), t)
+	AssertEqual(stack.Count(), 2, t)
+	valuePeek, _ := stack.Peek()
 	AssertEqual(valuePeek, 456, t)
 
-	valuePop, _ := stack.pop()
+	valuePop, _ := stack.Pop()
 	AssertEqual(valuePop, 456, t)
-	AssertFalse(stack.isEmpty(), t)
-	AssertEqual(stack.count(), 1, t)
-	valuePeek2, _ := stack.peek()
+	AssertFalse(stack.IsEmpty(), t)
+	AssertEqual(stack.Count(), 1, t)
+	valuePeek2, _ := stack.Peek()
 	AssertEqual(valuePeek2, 123, t)
 
-	valuePop2, _ := stack.pop()
+	valuePop2, _ := stack.Pop()
 	AssertEqual(valuePop2, 123, t)
-	AssertTrue(stack.isEmpty(), t)
-	AssertEqual(stack.count(), 0, t)
-	_, validPeek := stack.peek()
+	AssertTrue(stack.IsEmpty(), t)
+	AssertEqual(stack.Count(), 0, t)
+	_, validPeek := stack.Peek()
 	AssertFalse(validPeek, t)
-	_, validPop2 := stack.pop()
+	_, validPop2 := stack.Pop()
 	AssertFalse(validPop2, t)
 }
 
 func TestMakeEmpty(t *testing.T) {
 	stack := Stack[int]{}
 
-	stack.push(123)
-	stack.push(456)
-	_, validPop := stack.pop()
+	stack.Push(123)
+	stack.Push(456)
+	_, validPop := stack.Pop()
 	AssertTrue(validPop, t)
-	_, validPop2 := stack.pop()
+	_, validPop2 := stack.Pop()
 	AssertTrue(validPop2, t)
-	_, validPop3 := stack.pop()
+	_, validPop3 := stack.Pop()
 	AssertFalse(validPop3, t)
 
-	stack.push(789)
-	AssertEqual(stack.count(), 1, t)
-	valuePeek, _ := stack.peek()
+	stack.Push(789)
+	AssertEqual(stack.Count(), 1, t)
+	valuePeek, _ := stack.Peek()
 	AssertEqual(valuePeek, 789, t)
 
-	valuePop, _ := stack.pop()
+	valuePop, _ := stack.Pop()
 	AssertEqual(valuePop, 789, t)
-	AssertTrue(stack.isEmpty(), t)
-	AssertEqual(stack.count(), 0, t)
-	_, validPeek := stack.peek()
+	AssertTrue(stack.IsEmpty(), t)
+	AssertEqual(stack.Count(), 0, t)
+	_, validPeek := stack.Peek()
 	AssertFalse(validPeek, t)
-	_, validPop4 := stack.pop()
+	_, validPop4 := stack.Pop()
 	AssertFalse(validPop4, t)
 }
